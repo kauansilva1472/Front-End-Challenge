bb075137eadb9ad18a1249e78637c6fbc8acccf0
kauansilva1472 <kauansilva1465285@gmail.com>
2026-05-21 12:09:24 -0300

    feat: adiciona interatividade e efeitos visuais no resgate

---
+++ JavaScript/Resgate.js
@@ -0,0 +1,7 @@
+const formulario = document.querySelector('.form-resgate');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    alert('Resgate realizado com sucesso!');
+});
